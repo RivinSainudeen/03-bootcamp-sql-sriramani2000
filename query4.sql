307bcfb028c8207823cd9a0cea666cd658adb270
RivinSainudeen <47632884+RivinSainudeen@users.noreply.github.com>
2021-06-19 19:37:31 +0530

    Updated - Distinctly

--- query4.sql
+++ query4.sql
@@ -1,3 +1,3 @@
-SELECT s.name
-FROM subjects s, books b, books_subjects bs
-WHERE b.id = bs.book AND s.id = bs.subject AND b.name='Atomic Habits';
+SELECT DISTINCT(s.name)
+FROM subjects s,books_subjects d
+WHERE s.id IN (SELECT d.subject FROM books_subjects d WHERE d.book =  (SELECT id FROM books WHERE title = "Atomic Habits"));
