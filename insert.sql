c6f9a7c318bdca921d9e52fd9bd7ceb1e5e3f839
sriramani2000 <78255842+sriramani2000@users.noreply.github.com>
2021-06-19 15:15:08 +0400

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,43 @@
+INSERT INTO publisher(name,country) VALUES('PHI','India');
+INSERT INTO publisher(name,country) VALUES('Harper','USA');
+INSERT INTO publisher(name,country) VALUES('GCP','USA');
+INSERT INTO publisher(name,country) VALUES('Avery','USA');
+INSERT INTO publisher(name,country) VALUES('Del Rey','UK');
+INSERT INTO publisher(name,country) VALUES('Vintage','UK');
+
+INSERT INTO books(title) VALUES('The C Programming Language');
+INSERT INTO books(title) VALUE('The Go Programming Language');
+INSERT INTO books(title) VALUES('The UNIX Programming Environment');
+INSERT INTO books(title) VALUES('Cryptonomicon');
+INSERT INTO books(title) VALUES('Deep Work');
+INSERT INTO books(title) VALUES('Atomic Habits');
+INSERT INTO books(title) VALUES('The City and The City');
+INSERT INTO books(title) VALUES('The Great War for Civilisation');
+
+INSERT INTO subjects(name) VALUES('C');
+INSERT INTO subjects(name) VALUES('UNIX');
+INSERT INTO subjects(name) VALUES('Technology');
+INSERT INTO subjects(name) VALUES('Go');
+INSERT INTO subjects(name) VALUES('Science Fiction');
+INSERT INTO subjects(name) VALUES('Productivity');
+INSERT INTO subjects(name) VALUES('Psychology');
+INSERT INTO subjects(name) VALUES('Politics');
+INSERT INTO subjects(name) VALUES('History');
+
+INSERT INTO books_subjects(book,subject) VALUES (1,1);
+INSERT INTO books_subjects(book,subject) VALUES (1,2);
+INSERT INTO books_subjects(book,subject) VALUES (1,3);
+INSERT INTO books_subjects(book,subject) VALUES (2,4);
+INSERT INTO books_subjects(book,subject) VALUES (2,3);
+INSERT INTO books_subjects(book,subject) VALUES (3,2);
+INSERT INTO books_subjects(book,subject) VALUES (3,3);
+INSERT INTO books_subjects(book,subject) VALUES (4,3);
+INSERT INTO books_subjects(book,subject) VALUES (4,5);
+INSERT INTO books_subjects(book,subject) VALUES (5,3);
+INSERT INTO books_subjects(book,subject) VALUES (5,6);
+INSERT INTO books_subjects(book,subject) VALUES (6,6);
+INSERT INTO books_subjects(book,subject) VALUES (6,7);
+INSERT INTO books_subjects(book,subject) VALUES (7,5);
+INSERT INTO books_subjects(book,subject) VALUES (7,8);
+INSERT INTO books_subjects(book,subject) VALUES (8,8);
+INSERT INTO books_subjects(book,subject) VALUES (8,9);
