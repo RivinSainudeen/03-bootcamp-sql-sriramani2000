08cf49b7e5daac30d435d066a8430155c3fb0a87
RivinSainudeen <47632884+RivinSainudeen@users.noreply.github.com>
2021-06-19 19:34:31 +0530

    Updated - right table

--- delete1.sql
+++ delete1.sql
@@ -1,4 +1,4 @@
-DELETE FROM subjects 
-WHERE name='History'; 
-DELETE FROM books_subjects 
-WHERE subject = 8;
+DELETE FROM books_subjects
+WHERE books_subjects.subject = (SELECT id FROM subjects WHERE name = "History");
+DELETE FROM subjects
+WHERE name = "History";
